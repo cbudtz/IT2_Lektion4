ff29595023d264855702a38ca1298052a258ee7b
cbudtz <chrbudtz@gmail.com>
2020-02-23 18:36:29 +0100

    button Deklarative style

---
+++ src/main/java/GuiController.java
@@ -0,0 +1,11 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class GuiController {
+    public Label label2;
+
+    public void buttonPressed(MouseEvent mouseEvent) {
+        label2.setText("Du trykkede på knappen!");
+    }
+}
