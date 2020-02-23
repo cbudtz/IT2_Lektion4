55b290353ed899fb97d79870c7a3ee8e7f87e589
cbudtz <chrbudtz@gmail.com>
2020-02-23 18:33:18 +0100

    button Imperative style

--- src/main/java/JavaFXApp.java
+++ src/main/java/JavaFXApp.java
@@ -1,7 +1,10 @@
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.geometry.Orientation;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
@@ -16,12 +19,19 @@ public class JavaFXApp extends Application {
     public void start(Stage primaryStage) {
         Label l = new Label("Hello, JavaFX!");
         Label l2 = new Label("Its a fine day!");
+        Button b = new Button("Push me");
+        b.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                l2.setText("You clicked the button!");
+            }
+        });
         FlowPane flowPane = new FlowPane(Orientation.VERTICAL);
         flowPane.getChildren().add(l);
         flowPane.getChildren().add(l2);
+        flowPane.getChildren().add(b);
         Scene scene = new Scene(flowPane,640,480);
         primaryStage.setScene(scene);
         primaryStage.show();
-
     }
 }
